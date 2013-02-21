51b73da083a754ebfbe2ae3e9b955917bd6340b4
Bo-Yi Wu <appleboy.tw@gmail.com>
2013-02-21 14:00:59 +0800

    install npm, UglifyJS and sqwish tools
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- install.sh
+++ install.sh
@@ -1,1 +1,21 @@
 #!/usr/bin/env bash
+
+##
+# install npm command
+##
+
+which npm 1> /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo '=> install nvm tool'
+    curl https://raw.github.com/appleboy/nvm/develop/install.sh | sh
+    . ~/.nvm/nvm.sh # This loads NVM
+    echo '=> install node.js stable version'
+    nvm install stable
+    echo '=> use node.js stable version'
+    nvm use stable
+fi
+
+echo '=> install UglifyJS 2 node-based js compressor.'
+npm install -g uglify-js
+echo '=> install sqwish node-based CSS compressor.'
+npm install -g sqwish
